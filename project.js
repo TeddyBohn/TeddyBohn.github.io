213fe4469b6ef3b4040321aa2a92b984d2deb541
nonon <166867949+TeddyBohn@users.noreply.github.com>
2024-05-24 12:57:04 -0700

    Update project.js

--- project.js
+++ project.js
@@ -1,1 +1,15 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+function multiply(num1, num2) {
+  let result = num1 * num2;
+  return result;
+}
+
+console.log(multiply(2, 5)); 
+
+document.querySelector("img").addEventListener("click", function () {
+  alert("Ouch! Stop poking me!");
+});
+
+document.getElementById("idName"); 
 
