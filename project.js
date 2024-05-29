0f8a421470584d2c74b24f264e52a0a4a9fe65cb
nonon <166867949+TeddyBohn@users.noreply.github.com>
2024-05-29 12:48:19 -0700

    Update project.js

--- project.js
+++ project.js
@@ -1,7 +1,7 @@
 const myImage = document.querySelector("img");
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
+  if (mySrc === "https://upload.wikimedia.org/wikipedia/commons/a/ab/Firefox_Nightly_logo%2C_2017.svg") {
     myImage.setAttribute("src", "images/firefox2.jfif");
   } else {
     myImage.setAttribute("src", "images/firefox-icon.png");
