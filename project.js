a9c500f208e446c4ee5bfd77fad42ea3fd11eb53
nonon <166867949+TeddyBohn@users.noreply.github.com>
2024-05-29 12:47:35 -0700

    Update project.js

--- project.js
+++ project.js
@@ -1,16 +1,9 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
-function multiply(num1, num2) {
-  let result = num1 * num2;
-  return result;
-}
-
-console.log(multiply(2, 5)); 
-
-document.querySelector("img").addEventListener("click", function () {
-  alert("Ouch! Stop poking me!");
-});
-
-document.getElementById("idName"); 
-
-
+const myImage = document.querySelector("img");
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.jfif");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
